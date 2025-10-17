e207369d1347f8273b08d430f05a841a79f57ada
elidegen <elidegen@icloud.com>
2025-10-17 12:53:10 +0200

    implement monad

--- src/main/scala/chess.scala
+++ src/main/scala/chess.scala
@@ -3,13 +3,11 @@ import scala.io.StdIn.readLine
 object Schach:
     def main(args:Array[String]) =
         println("\nWelcome to Chess?")
-        try
-            args(0).toInt
-            println(createChessBoard(args(0).toInt))
-        catch
-            case _: Any =>
-                val size = getSize()
-                println(createChessBoard(size))
+        val size =
+        args.headOption
+            .flatMap(_.toIntOption)
+            .getOrElse(getSize())
+        println(createChessBoard(size))
 
     def createChessBoard(size:Int):String = ("+" + " - +" * size + "\n" + "|   "* size + "|\n")* (size) + "+" + " - +" * size
 
