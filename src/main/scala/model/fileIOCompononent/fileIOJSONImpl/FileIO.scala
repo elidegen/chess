93ddcbae05ea1a87a18c9e417526450672e8ef8e
elidegen <elidegen@icloud.com>
2026-01-16 08:41:06 +0100

    rebase

---
+++ src/main/scala/model/fileIOCompononent/fileIOJSONImpl/FileIO.scala
@@ -0,0 +1,3 @@
+package model.fileIOCompononent.fileIOJSONImpl
+
+
---
+++ src/main/scala/model/fileIOCompononent/fileIOXMLImpl/FileIO.scala
@@ -0,0 +1,5 @@
+package model.fileIOCompononent.fileIOXMLImpl
+
+class FileIO extends FileIOInterface {
+  override def load: GameContext
+}
